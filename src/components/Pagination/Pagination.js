bf09d8454b6d288f5a3e5808014383daeafac976
scott fuston <fuston05@yahoo.com>
2021-01-25 17:30:57 -0500

    fixes a '==' typo

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -2,24 +2,35 @@
 import React from "react";
 
 export const Pagination = ({ page, setPage, totalPages }) => {
-
   const prevPage = () => {
-    if ((page - 1) > 0) {
-      setPage(page-1)
+    if (page - 1 > 0) {
+      setPage(page - 1);
     }
-  }
+  };
 
   const nextPage = () => {
-    if ((page + 1) < Math.ceil(totalPages/10)) {
-      setPage(page +1);
+    if (page + 1 < Math.ceil(totalPages / 10)) {
+      setPage(page + 1);
     }
-  }
+  };
 
   return (
     <div className="paginationCont">
-      <p className= {`paginationButton ${page == 1 ? 'isDisabled' : null}`} onClick= {() => prevPage()}>&laquo; Prev</p>
+      <p
+        className={`paginationButton ${page === 1 ? "isDisabled" : null}`}
+        onClick={() => prevPage()}
+      >
+        &laquo; Prev
+      </p>
       <p> {page} </p>
-      <p className= {`paginationButton ${page +1 === Math.ceil(totalPages/10) ? 'isDisabled' : null}`} onClick= {() => nextPage()}>Next &raquo;</p>
+      <p
+        className={`paginationButton ${
+          page + 1 === Math.ceil(totalPages / 10) ? "isDisabled" : null
+        }`}
+        onClick={() => nextPage()}
+      >
+        Next &raquo;
+      </p>
     </div>
   );
 };
